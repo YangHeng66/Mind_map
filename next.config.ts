5223c33f9ee203f09e9aa67da4ecbcfcd8997de3
YangHeng66 <yangheng66@gmail.com>
2025-02-26 14:59:05 +0800

    Remove ESLint config and ignore ESLint errors during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 在构建过程中忽略ESLint错误
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
